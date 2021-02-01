d5235c44da19338990dd8a882897e0b1fb20fabd
Aby Dahana <abydahana@gmail.com>
2021-02-02 05:50:33 +0700

    Initial commit

--- themes/backend/layout.php
+++ themes/backend/layout.php
@@ -26,15 +26,6 @@
 		
 		<?php echo aksara_header(); ?>
 		
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-		<style type="text/css">
-			body
-			{
-				font-family: 'Fira Sans', sans-serif
-			}
-		</style>
-		
 		<script type="text/javascript">(function(w,d,u){w.readyQ=[];w.bindReadyQ=[];function p(x,y){if(x=="ready"){w.bindReadyQ.push(y)}else{w.readyQ.push(x)}};var a={ready:p,bind:p};w.$=w.jQuery=function(f){if(f===d||f===u){return a}else{p(f)}}})(window,document)</script>
 	</head>
 	<body>
--- themes/default/layout.php
+++ themes/default/layout.php
@@ -15,15 +15,6 @@
 		<?php echo aksara_header(); ?>
 		
 		<?php echo asset_loader('css/styles.css'); ?>
-		
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-		<style type="text/css">
-			body
-			{
-				font-family: 'Fira Sans', sans-serif
-			}
-		</style>
 	</head>
 	<body>
 		
