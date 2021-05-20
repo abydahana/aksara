6c49ac4467ef4710e1ea01ba0c7bc5e8434eafac
Aby Dahana <abydahana@gmail.com>
2021-05-21 04:43:55 +0700

    Minor update
    A minor update to fix infinity redirect after sign in with OAuth when site were in demo mode.

--- aksara/Modules/Auth/Controllers/Auth.php
+++ aksara/Modules/Auth/Controllers/Auth.php
@@ -309,7 +309,7 @@ class Auth extends \Aksara\Laboratory\Core
 	{
 		if(DEMO_MODE)
 		{
-			return throw_exception(403, phrase('this_feature_is_disabled_in_demo_mode'), current_page());
+			return throw_exception(403, phrase('this_feature_is_disabled_in_demo_mode'), current_page('../'));
 		}
 		
 		if($session)
