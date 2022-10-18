345b95840021219cb6e551e27b45fdce1e534492
Aby Dahana <abydahana@gmail.com>
2022-10-18 17:41:19 +0700

    Add the version changes

--- aksara/Common.php
+++ aksara/Common.php
@@ -20,7 +20,7 @@ if(! function_exists('aksara'))
 	 */
 	function aksara($parameter = null)
 	{
-		$version									= '4.5.0';
+		$version									= '4.5.2';
 		$modified									= filemtime(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Laboratory' . DIRECTORY_SEPARATOR . 'Core.php');
 		
 		if('version' == $parameter)
