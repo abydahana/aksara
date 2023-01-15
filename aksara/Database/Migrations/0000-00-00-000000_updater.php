4be1ca1caee8170779e481d041b9c24c747cb93f
Aby Dahana <abydahana@gmail.com>
2023-01-15 18:05:56 +0700

    Add the new table to the migration

--- aksara/Database/Migrations/0000-00-00-000000_updater.php
+++ aksara/Database/Migrations/0000-00-00-000000_updater.php
@@ -16,6 +16,42 @@ class Updater extends Migration
     public function up()
     {
 		// modify or create new table properties
+		
+		$this->forge->addField
+		(
+			array
+			(
+				'user_id' => array
+				(
+					'type' => 'bigint',
+					'constraint' => 22,
+					'unsigned' => true,
+					'null' => false
+				),
+				'post_id' => array
+				(
+					'type' => 'bigint',
+					'constraint' => 22,
+					'unsigned' => true,
+					'null' => false
+				),
+				'post_type' => array
+				(
+					'type' => 'varchar',
+					'constraint' => 20,
+					'null' => false
+				),
+				'timestamp' => array
+				(
+					'type' => 'timestamp',
+					'null' => false
+				)
+			)
+		);
+		$this->forge->addKey('user_id', false, false);
+		$this->forge->addKey('post_id', false, false);
+		$this->forge->addForeignKey('user_id', 'app__users', 'user_id', ($this->db->DBDriver == 'SQLSRV' ? 'NO ACTION' : 'CASCADE'), ($this->db->DBDriver == 'SQLSRV' ? 'NO ACTION' : 'RESTRICT'));
+		$this->forge->createTable('post__likes');
     }
 	
 	public function down()
