711e0733d008f0306b0784f23c3d870dbaf4daf4
Aby Dahana <abydahana@gmail.com>
2023-11-24 23:42:41 +0700

    Fixing the example of user's individual privileges

--- aksara/Modules/Administrative/Views/users/privileges/form.php
+++ aksara/Modules/Administrative/Views/users/privileges/form.php
@@ -1,36 +1,30 @@
 <div class="container-fluid py-3">
     <form action="<?= current_page(); ?>" method="POST" class="--validate-form" enctype="multipart/form-data">
-        <div class="row">
-            <div class="col-md-2">
+        <div class="row align-items-center">
+            <div class="col-sm-3 col-md-2 text-center">
                 <img src="<?= get_image('users', $userdata->photo, 'thumb'); ?>" class="img-fluid rounded" alt="..." />
             </div>
-            <div class="col-md-8">
+            <div class="col-sm-9 col-md-8">
                 <div class="row">
-                    <label class="col-4 col-md-3 text-muted">
-                        <?= phrase('User ID'); ?>
-                    </label>
-                    <label class="col-8 col-md-9">
-                        <?= $userdata->user_id; ?>
-                    </label>
-                </div>
-                <div class="row">
-                    <label class="col-4 col-md-3 text-muted">
+                    <label class="col-sm-4 col-md-3 text-muted">
                         <?= phrase('Username'); ?>
                     </label>
-                    <label class="col-8 col-md-9">
+                    <label class="col-sm-8 col-md-9">
                         <?= $userdata->username; ?>
                     </label>
                 </div>
+                <hr class="my-2" />
                 <div class="row">
-                    <label class="col-4 col-md-3 text-muted">
+                    <label class="col-sm-4 col-md-3 text-muted">
                         <?= phrase('Full Name'); ?>
                     </label>
                     <label class="col-8 col-md-9">
                         <?= $userdata->first_name . ' ' . $userdata->last_name; ?>
                     </label>
                 </div>
+                <hr class="my-2" />
                 <div class="row">
-                    <label class="col-4 col-md-3 text-muted">
+                    <label class="col-sm-4 col-md-3 text-muted">
                         <?= phrase('User Group'); ?>
                     </label>
                     <label class="col-8 col-md-9">
@@ -38,11 +32,12 @@
                     </label>
                 </div>
                 <?php if ($year): ?>
+                <hr class="my-2" />
                 <div class="row">
-                    <label class="col-4 col-md-3 text-muted">
+                    <label class="col-sm-4 col-md-3 text-muted">
                         <?= phrase('Access Year'); ?>
                     </label>
-                    <label class="col-8 col-md-4">
+                    <label class="col-sm-8 col-md-4">
                         <?php
                             $options = null;
 
@@ -58,22 +53,28 @@
                 <?php endif; ?>
             </div>
         </div>
-        <hr class="row" />
+        <hr />
         <div class="row">
-            <div class="col-md-5">
+            <div class="col-sm-6 col-md-5">
                 <div class="mb-3">
                     <label class="text-muted d-block" for="menus_input">
                         <?= phrase('Accessible Menus'); ?>
                     </label>
-                    <?= $visible_menu; ?>
+                    <?php if ($visible_menu): ?>
+                        <?= $visible_menu; ?>
+                    <?php else: ?>
+                        <div class="alert alert-warning">
+                            <?= phrase('No menu is available right now'); ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
-            <?php if ($sub_level_1): ?>
-                <div class="col-md-5">
-                    <div class="form-group mb-3">
-                        <label class="text-muted d-block" for="kegiatan_input">
-                            <?= phrase('The sub level can be accessed'); ?>
-                        </label>
+            <div class="col-sm-6 col-md-5">
+                <div class="mb-3">
+                    <label class="text-muted d-block" for="sub_level_1_input">
+                        <?= phrase('The sub level can be accessed'); ?>
+                    </label>
+                    <?php if ($sub_level_1): ?>
                         <?php
                             $option = null;
                             foreach($sub_level_1 as $key => $val) {
@@ -85,9 +86,13 @@
                         <select name="sub_level_1" class="form-control" id="sub_level_1_input" placeholder="<?= phrase('Please choose'); ?>">
                             <?= $option; ?>
                         </select>
-                    </div>
+                    <?php else: ?>
+                        <div class="alert alert-warning">
+                            <?= phrase('No sub level table assigned to the selection'); ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
-            <?php endif; ?>
+            </div>
         </div>
         <div class="opt-btn-overlap-fix"></div>
         <div class="row opt-btn">
