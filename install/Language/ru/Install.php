12d133a243da4f63807c153278f3a9bae63b42e2
Aleksandr <seoinetru@gmail.com>
2024-01-21 01:57:19 +0300

    Create Install.php
    Added Russian language

---
+++ install/Language/ru/Install.php
@@ -0,0 +1,208 @@
+<?php
+
+/**
+ * Этот файл является частью Aksara CMS, как фреймворка, так и платформы для публикаций.
+ *
+ * @author     Aby Dahana <abydahana@gmail.com>
+ * @copyright  (c) Aksara Laboratory <https://aksaracms.com>
+ * @license    MIT License
+ *
+ * Этот исходный файл подпадает под лицензию MIT, которая включена
+ * в этот исходный код в файле LICENSE.txt.
+ *
+ * Когда приходят знамения, те, кто не верит в "то время",
+ * имеют только два выбора: покончить с собой или стать жестокими.
+ */
+
+return [
+    // Глобальные
+    'Aksara Installer' => 'Установщик Aksara',
+    'Whoops!' => 'Упс!',
+    'Back' => 'Назад',
+    'Refresh' => 'Обновить',
+    'Continue' => 'Продолжить',
+    'DSN' => 'DSN',
+    'Hostname' => 'Хост',
+    'Port' => 'Порт',
+    'Username' => 'Имя пользователя',
+    'Password' => 'Пароль',
+    'Confirm Password' => 'Подтвердите пароль',
+    'Available' => 'Доступно',
+
+    // Индекс
+    'Checking Requirements' => 'Проверка требований',
+    'Database Configuration' => 'Настройка базы данных',
+    'Security Configuration' => 'Настройка безопасности',
+    'System Configuration' => 'Настройка системы',
+    'Finalizing' => 'Завершение',
+    'Hello there' => 'Привет',
+    'Choose Language' => 'Выберите язык',
+    'Thank you for choosing Aksara' => 'Спасибо, что выбрали <a href="//aksaracms.com" class="text-primary fw-bold" target="_blank">Aksara</a>!',
+    'Before we start the installation, please take a moment to read this few notes.' => 'Прежде чем мы начнем установку, прочтите несколько замечаний.',
+    'You could check the agreement box and skip reading as usual.' => 'Вы можете поставить галочку в "<b>Согласие</b>" и пропустить чтение, как обычно, но мы всё равно верим, что есть "<b>Задрот</b>", который будет читать эти замечания предложение за предложением.',
+
+    'Article 1' => '<a href="//aksaracms.com" class="text-primary fw-bold" target="_blank">Aksara</a> - всего лишь инструмент для создания экосистем по вашим потребностям. Но то, что вы построили с <a href="//aksaracms.com" class="text-primary fw-bold" target="_blank">Aksara</a>, должно соответствовать ей;',
+
+    'Article 2' => 'Вы имеете право распространять экосистемы, построенные с использованием <a href="//aksaracms.com" class="text-primary fw-bold" target="_blank">Aksara</a>, без каких-либо ограничений моего разрешения, однако признание, что они были построены вами в целом, стыдно;',
+
+    'Article 3' => 'Не подводите творческих людей, которые делятся своей работой бесплатно, иначе вы обнаружите, что они продают свою будущую идею по ценам, которые вы не можете позволить себе.',
+
+    'Three notes should be enough.' => 'Три замечания должны быть достаточными.',
+    'I look forward to your support.' => 'Мы ждем вашей поддержки.',
+    'The fool' => 'Дурак',
+    'Pretend to agree' => 'Притворитесь согласными',
+    'Start Installation' => 'Начать установку',
+
+    // База данных
+    'Fill the requested fields below with your database connection.' => 'Заполните запрошенные поля ниже вашим подключением к базе данных.',
+    'Please fill all required fields!' => 'Пожалуйста, заполните все обязательные поля!',
+    'Database Driver' => 'Драйвер базы данных',
+    'Initial Database' => 'Исходная база данных',
+    'Please choose the correct database driver!' => 'Пожалуйста, выберите правильный драйвер базы данных!',
+    'Create database if not exist' => 'Создать базу данных, если не существует',
+
+    // Завершение
+    'The timezone is not valid!' => 'Неверный часовой пояс!',
+    'The site title cannot be empty!' => 'Заголовок сайта не может быть пустым!',
+    'The site description cannot be empty!' => 'Описание сайта не может быть пустым!',
+    'File extension must be comma separated alphanumeric only!' => 'Расширение файла должно содержать только буквенно-цифровые символы, разделенные запятой!',
+    'Image extension must be comma separated alphanumeric only!' => 'Расширение изображения должно содержать только буквенно-цифровые символы, разделенные запятой!',
+    'Maximum upload size must be numeric only!' => 'Максимальный размер загрузки должен быть только числовым!',
+    'Image dimension must be numeric only!' => 'Размер изображения должен быть только числовым!',
+    'Thumbnail dimension must be numeric only!' => 'Размер миниатюры должен быть только числовым!',
+    'Icon dimension must be numeric only!' => 'Размер иконки должен быть только числовым!',
+    'All catched up!' => 'Все готово!',
+    'Your application is ready to install using provided settings.' => 'Ваше приложение готово к установке с использованием предоставленных настроек.',
+    'Just one more step' => 'Остался всего один шаг,',
+    'Make sure what you filled in on the previous form is correct.' => 'Убедитесь, что вы правильно заполнили предыдущую форму.',
+    'Once you have successfully run the installer, there is no more back button.' => 'После успешного запуска установщика нет кнопки "Назад".',
+    'Click run installer to applying your configuration.' => 'Щелкните "Запустить установщик", чтобы применить вашу конфигурацию.',
+    'Run Installer' => 'Запустить установщик',
+
+    // Установка
+    'Failed to open or create the configuration file!' => 'Не удалось открыть или создать файл конфигурации!',
+    'Failed to write the configuration into file!' => 'Не удалось записать конфигурацию в файл!',
+    'Congratulations!' => 'Поздравляю!',
+    'Aksara has been successfully installed on your system!' => '<a href="//aksaracms.com" class="text-primary fw-bold" target="_blank">Aksara</a> успешно установлен на вашей системе!',
+    'Notice!' => 'Уведомление!',
+    'Your configuration file or folder is not writable or there was a problem while creating the configuration file.' => 'Ваш файл или папка конфигурации не доступны для записи, или произошла проблема при создании файла конфигурации.',
+    'You will have to create the following code by hand manually and locate to the following directory:' => 'Вы должны создать следующий код вручную и разместить в следующей директории:',
+    'Revalidate configuration after the configuration file is created or uploaded.' => 'Повторно проверьте конфигурацию после создания или загрузки файла конфигурации.',
+    'You can login as superuser using following credential:' => 'Вы можете войти как суперпользователь, используя следующие учетные данные:',
+    'Follow our updates to get our other works if you find this useful.' => 'Следите за нашими обновлениями, чтобы узнать о наших других работах, если вам это полезно.',
+    'Just to remind you, we also collect donations from people like you to support our research.' => 'Просто напоминаем, что мы также принимаем пожертвования от людей, таких как вы, чтобы поддерживать наши исследования.',
+    'Regardless of the amount will be very useful.' => 'Независимо от суммы, она будет очень полезной.',
+    'Cheers' => 'Вашему здоровью',
+    'Launch Site' => 'Запустить ваш сайт',
+    'Revalidate Configuration' => 'Повторно проверить конфигурацию',
+
+    // Требования
+    'You just need to pretend to agree!' => 'Вам просто нужно притвориться, что вы согласны!',
+    'Awesome!' => 'Отлично!',
+    'You just read our notes and pretend to agree with it.' => 'Вы только что прочли наши замечания и притворились, что согласны с ними :)',
+    'We will help you to prepare your application using this installation wizard.' => 'Мы поможем вам подготовить ваше приложение с помощью этого мастера установки.',
+    'Before you go, make sure this pre-requirements are fulfilled without any warning.' => 'Прежде чем начать, убедитесь, что все предварительные требования выполнены без предупреждений.',
+    'Otherwise your application will not work properly.' => 'В противном случае ваше приложение не будет работать должным образом.',
+    'PHP Version' => 'Версия PHP',
+    'Rewrite Module' => 'Модуль перезаписи',
+    'Internationalization' => 'Интернационализация',
+    'Multibyte String' => 'Многобайтовая строка',
+    'PHP GD' => 'PHP GD',
+    'JSON' => 'JSON',
+    'XML' => 'XML',
+    'The minimum required version is' => 'Минимально требуемая версия',
+    'and the maximum version is' => 'а максимальная версия',
+    'Disabled' => 'Отключено',
+    'Enabled' => 'Включено',
+    'Off' => 'Выключено',
+    'On' => 'Включено',
+    'Turn it on!' => 'Включите это!',
+    'Optional' => 'Необязательно',
+    'Some requirement are not yet fulfilled.' => 'Некоторые требования еще не выполнены.',
+    'Please update your server configuration and click on refresh button to continue the installation.' => 'Обновите конфигурацию вашего сервера и нажмите кнопку "Обновить", чтобы продолжить установку.',
+    'The rewrite module is disabled by your server.' => 'Модуль перезаписи отключен вашим сервером.',
+    'You can continue the installation but we recommend to enable it.' => 'Вы можете продолжить установку, но мы рекомендуем включить его.',
+
+  // Безопасность
+    'Enter your secret formula to secure your application.' => 'Введите вашу секретную формулу для обеспечения безопасности вашего приложения.',
+    'Encryption Key' => 'Encryption Key',
+    'Your encryption key' => 'Ваш ключ шифрования',
+    'Cookie Name' => 'Cookie Name',
+    'Session Expiration' => 'Истечение сессии',
+    'The number of seconds you want the session to last.' => 'Количество секунд, на которое вы хотите, чтобы сессия длилась. Установка в 0 (ноль) означает истечение при закрытии браузера.',
+    'In seconds' => 'В секунде(ах)',
+    'Unique cookie name to prevent session conflict' => 'Уникальное имя cookie, чтобы предотвратить конфликты сессий',
+    'Superuser' => 'Суперпользователь',
+    'First Name' => 'Имя',
+    'Last Name' => 'Фамилия',
+    'Email Address' => 'Адрес электронной почты',
+    'Create username for superuser' => 'Создать имя пользователя для суперпользователя',
+    'Password for superuser' => 'Пароль для суперпользователя',
+    'Retype the password' => 'Повторите пароль',
+
+// Система
+    'The encryption key cannot be empty!' => 'Ключ шифрования не может быть пустым!',
+    'The encryption key contain unsupported character!' => 'Ключ шифрования содержит неподдерживаемый символ!',
+    'The cookie name cannot be empty!' => 'Имя cookie не может быть пустым!',
+    'The cookie name contain unsupported character!' => 'Имя cookie содержит неподдерживаемый символ!',
+    'The first name cannot be empty!' => 'Имя не может быть пустым!',
+    'Please enter your valid email address!' => 'Пожалуйста, введите ваш действительный адрес электронной почты!',
+    'The username must be in alphanumeric only!' => 'Имя пользователя должно содержать только буквенно-цифровые символы и состоять из минимум 5 символов!',
+    'The password cannot be empty!' => 'Пароль не может быть пустым!',
+    'The password confirmation did not match!' => 'Подтверждение пароля не совпадает!',
+    'Enter the basic system configuration.' => 'Введите основную конфигурацию системы.',
+    'You will able to change it after the installation.' => 'Вы сможете изменить его после установки.',
+    'Installation Mode' => 'Режим установки',
+    'Basic (with sample)' => 'Основной (с образцом)',
+    'Developer (without sample)' => 'Разработчик (без образца)',
+    'Timezone' => 'Часовой пояс',
+    'Site Settings' => 'Настройки сайта',
+    'Site Title' => 'Заголовок сайта',
+    'Enter the site title' => 'Введите заголовок сайта',
+    'Site Description' => 'Описание сайта',
+    'Enter the site description' => 'Введите описание сайта',
+    'Upload Settings' => 'Настройки загрузки',
+    'Allowed File Extension' => 'Разрешенные расширения файлов',
+    'Allowed Image Extension' => 'Разрешенные расширения изображений',
+    'Separate with comma' => 'Разделять запятой',
+    'Upload Limit' => 'Лимит загрузки',
+    'Image Width Dimension' => 'Размер изображения',
+    'Original' => 'Оригинал',
+    'Thumbnail' => 'Миниатюра',
+    'Icon' => 'Иконка',
+    'In pixel' => 'В пикселях',
+    'ZIP Archiver' => 'ZIP-архиватор',
+    'Your application is ready to install using the provided configurations.' => 'Ваше приложение готово к установке с предоставленной конфигурацией.',
+    'Agreement' => 'Лицензионное соглашение',
+    'Database Initial' => 'Исходная база данных',
+    'Email' => 'Электронная почта',
+    'Password Confirmation' => 'Подтверждение пароля',
+    'File Extension' => 'Разрешенные расширения файлов',
+    'Image Extension' => 'Разрешенные расширения изображений',
+    'Max Upload Size' => 'Максимальный размер загрузки',
+    'Image Dimension' => 'Размер изображения',
+    'Thumbnail Dimension' => 'Размер миниатюры',
+    'Icon Dimension' => 'Размер иконки',
+    'The installer was interrupted!' => 'Установщик был прерван!',
+    'The installer was unable to write the configuration file.' => 'Установщик не смог записать файл конфигурации.',
+    'Please follow the instructions below to continue.' => 'Пожалуйста, следуйте инструкциям ниже, чтобы продолжить.',
+    'Problem Found' => 'Обнаружена проблема',
+    'Unable to create or write file' => 'Невозможно создать или записать файл',
+    'Solution' => 'Решение',
+    'Please download the configuration file below and upload or paste it manually under the following directory' => 'Пожалуйста, загрузите файл конфигурации ниже и загрузите или вставьте его вручную в следующий каталог',
+    'Download Configuration' => 'Скачать конфигурацию',
+    'Once the configuration file were uploaded, please click the refresh button to continue the installation.' => 'После загрузки файла конфигурации нажмите кнопку "Обновить", чтобы продолжить установку.',
+    'Unable to extract the sample module.' => 'Не удалось извлечь образец модуля.',
+    'Make sure the following directory is writable' => 'Убедитесь, что следующий каталог доступен для записи',
+    'Skip installing the sample module' => 'Пропустить установку образца модуля.',
+
+// Разное
+    'Subscribe to my channel' => 'Подписаться на мой канал',
+    'Be my friend' => 'Будь моим другом',
+    'Follow my GitHub' => 'Подписаться на мой GitHub',
+    'Import sample data' => 'Установить с образцовыми данными',
+
+// Дополнительные фразы
+    'Sectoral' => 'Секторальное отображение',
+    'Regional' => 'Региональное отображение'
+];
