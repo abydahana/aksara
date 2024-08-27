ba5b84b29ec211ef200059a6a82b7e67aa86ee77
Aby Dahana <abydahana@gmail.com>
2024-08-27 23:41:46 +0700

    Refine people module view template

--- aksara/Modules/Peoples/Views/index.php
+++ aksara/Modules/Peoples/Views/index.php
@@ -1,53 +1,58 @@
-<?php
-    $output = null;
-?>
-<div class="py-3 py-md-5 bg-light">
-    <div class="container">
-        <h1 class="text-center text-md-start">
-            <?= $meta->title; ?>
-        </h1>
-        <p class="lead text-center text-md-start">
-            <?= truncate($meta->description, 256); ?>
-        </p>
-    </div>
-</div>
 <?php if ($results): ?>
-    <div class="py-3 py-md-5">
+    <div class="py-3 py-md-5 bg-light">
         <div class="container">
+            <h1 class="text-center text-md-start">
+                <?= $meta->title; ?>
+            </h1>
+            <p class="lead text-center text-md-start">
+                <?= truncate($meta->description, 256); ?>
+            </p>
+        </div>
+    </div>
+<?php endif; ?>
+
+<div class="py-3 py-md-5">
+    <div class="container">
+        <?php if ($results): ?>
             <div class="row">
                 <?php foreach ($results as $key => $val): ?>
-                    <div class="col-sm-6 col-md-4 col-lg-3">
-                        <div class="card border-0 rounded-4 shadow-sm mb-3 overflow-hidden">
+                    <div class="col-6 col-md-4 col-lg-3">
+                        <div class="mb-4 text-center">
                             <a href="<?= go_to($val->people_slug); ?>" class="--xhr">
-                                <img src="<?= get_image('peoples', $val->photo, 'thumb'); ?>" class="card-img-top w-100" alt="<?= $val->first_name . ' '  . $val->last_name; ?>" />
+                                <img src="<?= get_image('peoples', $val->photo, 'thumb'); ?>" class="img-fluid rounded-circle" alt="<?= $val->first_name . ' '  . $val->last_name; ?>" />
                             </a>
-                            <div class="card-body">
+                            <h3 class="text-truncate">
                                 <a href="<?= go_to($val->people_slug); ?>" class="--xhr">
-                                    <h5 class="card-title text-center text-truncate">
-                                        <?= truncate($val->first_name, 22) . ' ' . truncate($val->last_name, 22); ?>
-                                    </h5>
+                                    <?= truncate($val->first_name, 22) . ' ' . truncate($val->last_name, 22); ?>
                                 </a>
-                                <a href="<?= go_to($val->people_slug); ?>" class="--xhr">
-                                    <h6 class="card-subtitle fw-light text-center mb-2 text-muted text-truncate">
-                                        <?= truncate($val->position, 22); ?>
-                                    </h6>
+                            </h3>
+                            <h6 class="text-truncate">
+                                <a href="<?= go_to($val->people_slug); ?>" class="text-muted --xhr">
+                                    <?= truncate($val->position, 22); ?>
                                 </a>
-                            </div>
+                            </h6>
                         </div>
                     </div>
                 <?php endforeach; ?>
             </div>
             
             <?= pagination($pagination); ?>
-        </div>
-    </div>
-<?php else: ?>
-    <div class="py-3 py-md-5">
-        <div class="container">
-            <div class="text-muted">
-                <i class="mdi mdi-information-outline"></i>
-                <?= phrase('No people are available right now.'); ?>
+        <?php else: ?>
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <div class="py-5">
+                        <div class="text-center">
+                            <i class="mdi mdi-dropbox mdi-5x text-muted"></i>
+                        </div>
+                        <h2 class="text-center">
+                            <?= phrase('No people is found!'); ?>
+                        </h2>
+                        <p class="lead text-center mb-5">
+                            <?= phrase('No people is available at the moment.'); ?>
+                        </p>
+                    </div>
+                </div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
-<?php endif; ?>
+</div>
