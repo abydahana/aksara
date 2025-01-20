4240f7ca53dbc9b86be3da33bb43f0165b086b10
Aby Dahana <abydahana@gmail.com>
2025-01-20 11:33:08 +0700

    update: version update

--- aksara/Common.php
+++ aksara/Common.php
@@ -21,7 +21,7 @@ if (! function_exists('aksara')) {
      */
     function aksara(?string $parameter = null)
     {
-        $version = '5.1.7';
+        $version = '5.1.8';
 
         if ('version' == $parameter) {
             return $version;
