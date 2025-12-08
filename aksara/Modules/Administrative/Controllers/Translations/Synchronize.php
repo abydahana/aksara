ca9f054a4556573ad45557b4747d811d2fe721ca
Aby Dahana <abydahana@gmail.com>
2025-12-08 16:54:14 +0700

    feat: scan phrases from each file when do translation sync

--- aksara/Modules/Administrative/Controllers/Translations/Synchronize.php
+++ aksara/Modules/Administrative/Controllers/Translations/Synchronize.php
@@ -35,6 +35,8 @@ class Synchronize extends \Aksara\Laboratory\Core
     {
         helper('filesystem');
 
+        // Generate phrases from source code
+        $generated_phrases = $this->_generate_phrases_from_source();
         $languages = get_filenames(WRITEPATH . 'translations');
         $populated_phrases = [];
         $error = 0;
@@ -54,10 +56,16 @@ class Synchronize extends \Aksara\Laboratory\Core
                     // Merge phrases
                     $populated_phrases = array_merge($populated_phrases, json_decode($translation, true));
                 } catch (\Throwable $e) {
-                    /* failed to write file, throw an error exception */
+                    // Failed to read file
                     $error++;
                 }
             }
+
+            // Filter: Keep only phrases that are also found in generated phrases
+            // $populated_phrases = array_intersect_key($populated_phrases, $generated_phrases);
+
+            // Merge with generated phrases from source code
+            $populated_phrases = array_merge($populated_phrases, $generated_phrases);
 
             // Combine array value using its key
             $populated_phrases = array_combine(array_keys($populated_phrases), array_keys($populated_phrases));
@@ -73,23 +81,127 @@ class Synchronize extends \Aksara\Laboratory\Core
                     // Attempt to get the translation source
                     $translation = file_get_contents(WRITEPATH . 'translations' . DIRECTORY_SEPARATOR . $val);
                     $phrases = json_decode($translation, true);
+
+                    // Keep only phrases that are populated
+                    $phrases = array_intersect_key($phrases, $populated_phrases);
+
+                    // Merge missing phrases
                     $phrases = array_merge($populated_phrases, $phrases);
 
-                    /* sort and humanize the order of phrase */
+                    // Sort phrases by key
                     ksort($phrases);
 
                     file_put_contents(WRITEPATH . 'translations' . DIRECTORY_SEPARATOR . $val, json_encode($phrases, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE | JSON_UNESCAPED_UNICODE));
                 } catch (\Throwable $e) {
-                    /* failed to write file, throw an error exception */
+                    // Failed to read or write file
                     $error++;
                 }
             }
         }
 
         if ($error) {
-            return throw_exception(403, phrase('Translation synchronized!') . ' ' .  phrase('However, there are') . ' <b>' . number_format($error) . '</b> ' . phrase('translations were unsuccessful.'), current_page('../'));
+            return throw_exception(403, phrase('Translation synchronized, however there are {{total_errors}} translations were unsuccessful.', ['total_errors' => '<b>' . number_format($error) . '</b>']), current_page('../'));
         }
 
-        return throw_exception(301, '<b>' . (sizeof($languages) - 1) . '</b> ' . ((sizeof($languages) - 1) > 1 ? phrase('languages') : phrase('language')) . ' ' . phrase('was successfully updated.') . ' <b>' . number_format(sizeof($populated_phrases)) . '</b> ' . phrase('phrases was synchronized.'), current_page('../'));
+        return throw_exception(301, phrase('{{total_languages}} languages and {{total_phrases}} phrases was successfully synchronized.', ['total_languages' => '<b>' . number_format(sizeof($languages) - 1) . '</b>', 'total_phrases' => '<b>' . number_format(sizeof($populated_phrases)) . '</b>']), current_page('../'));
+    }
+
+    /**
+     * Generate phrases from source code by scanning phrase() functions
+     */
+    private function _generate_phrases_from_source()
+    {
+        $translations = [];
+        $directories = ['aksara', 'modules', 'themes'];
+        $fileExtensions = ['php', 'twig'];
+
+        foreach ($directories as $directory) {
+            if (! is_dir(ROOTPATH . $directory)) {
+                continue;
+            }
+
+            $this->_scan_directory(ROOTPATH . $directory, $fileExtensions, $translations);
+        }
+
+        return $translations;
+    }
+
+    /**
+     * Recursively scan directory for PHP and Twig files
+     */
+    private function _scan_directory($directory, $fileExtensions, &$translations)
+    {
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($directory, \RecursiveDirectoryIterator::SKIP_DOTS)
+        );
+
+        foreach ($iterator as $file) {
+            if ($file->isFile() && in_array($file->getExtension(), $fileExtensions)) {
+                $this->_scan_file($file->getPathname(), $translations);
+            }
+        }
+    }
+
+    /**
+     * Scan individual file for phrase() calls
+     */
+    private function _scan_file($filePath, &$translations)
+    {
+        $content = file_get_contents($filePath);
+
+        // Remove comments to avoid false positives
+        $content = preg_replace('/\/\*.*?\*\//s', '', $content); // Multi-line comments
+        $content = preg_replace('/\/\/.*?$/m', '', $content); // Single-line comments
+
+        // Pattern for single quotes: phrase('...')
+        if (preg_match_all("/phrase\s*\(\s*'((?:[^'\\\\]|\\\\.)*)'/", $content, $matches)) {
+            foreach ($matches[1] as $key) {
+                $this->_add_translation($key, $translations, $filePath);
+            }
+        }
+
+        // Pattern for double quotes: phrase("...")
+        if (preg_match_all('/phrase\s*\(\s*"((?:[^"\\\\]|\\\\.)*)"/s', $content, $matches)) {
+            foreach ($matches[1] as $key) {
+                $this->_add_translation($key, $translations, $filePath);
+            }
+        }
+    }
+
+    /**
+     * Add translation key if valid
+     */
+    private function _add_translation($key, &$translations, $filePath = '')
+    {
+        $original_key = $key;
+
+        // Manual unescape - only for escaped quotes
+        $key = str_replace("\\'", "'", $key);
+        $key = str_replace('\\"', '"', $key);
+
+        // Trim whitespace
+        $key = trim($key);
+
+        // Skip empty keys
+        if (empty($key)) {
+            return;
+        }
+
+        // Skip keys with suspicious patterns (likely parsing errors)
+        // Allow {{ }} for placeholders, but skip other suspicious patterns
+        if (strpos($key, '->') !== false ||
+            strpos($key, "('") !== false ||
+            strlen($key) > 500 ||
+            strpos($key, ';') !== false ||
+            preg_match('/\[.*\]/', $key) || // Only skip if has square brackets
+            strpos($key, "\n") !== false ||
+            strpos($key, "\r") !== false) {
+            return;
+        }
+
+        // Add to translations array
+        if (! isset($translations[$key])) {
+            $translations[$key] = $key;
+        }
     }
 }
