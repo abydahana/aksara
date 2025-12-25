5c69bd1a81de79eeff270f195daf831866e20094
Aby Dahana <abydahana@gmail.com>
2025-12-25 22:57:47 +0700

    chore: update application version to 5.2.5

--- aksara/Common.php
+++ aksara/Common.php
@@ -28,7 +28,7 @@ if (! function_exists('aksara')) {
      */
     function aksara(string $parameter): string
     {
-        $version = '5.2.4';
+        $version = '5.2.5';
 
         if ('version' == $parameter) {
             return $version;
