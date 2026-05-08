f6a5edbf1cc693973f5ab5fff3005122c2881271
Aby Dahana <abydahana@gmail.com>
2026-05-09 02:45:03 +0700

    Refine Home page layout and typography

--- modules/Home/Views/index.php
+++ modules/Home/Views/index.php
@@ -18,12 +18,6 @@
             <div class="d-flex justify-content-center gap-3 mb-5">
                 <a href="<?= base_url('auth'); ?>" class="btn btn-primary rounded-pill px-4 --modal">GET STARTED <i class="mdi mdi-arrow-right"></i></a>
             </div>
-            
-            <div class="row mt-5">
-                <div class="col-12 col-lg-10 offset-lg-1">
-                    <img src="<?= get_module_asset('images/hero.png'); ?>" class="img-fluid rounded-5" alt="Map Interface">
-                </div>
-            </div>
         </div>
     </div>
 </section>
