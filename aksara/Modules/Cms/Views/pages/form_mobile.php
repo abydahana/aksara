738bb8354003db93533a7061c6483bdba514369d
Aby Dahana <abydahana@gmail.com>
2026-08-14 19:15:40 +0700

    refactor: reorganize page builder toolbar layout and improve responsiveness with dedicated mobile view support.

---
+++ aksara/Modules/Cms/Views/pages/form_mobile.php
@@ -0,0 +1,19 @@
+<style>
+    [data-role="description"] {
+        display: none
+    }
+</style>
+
+<div class="container-fluid py-3">
+    <div class="alert alert-danger callout">
+        <div class="d-flex align-items-center">
+            <div class="flex-shrink-0">
+                <i class="mdi mdi-information-outline mdi-2x"></i>
+            </div>
+            <div class="flex-grow-1 ms-3">
+                <strong><?= phrase('Unsupported Device'); ?></strong>
+                <p class="mb-0"><?= phrase('Page Builder is only accessible using desktop browser.'); ?></p>
+            </div>
+        </div>
+    </div>
+</div>
