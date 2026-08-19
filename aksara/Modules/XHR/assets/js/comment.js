19f60201df66442682da39cba19d3558d9223f27
Aby Dahana <abydahana@gmail.com>
2026-08-19 18:20:19 +0700

    fix: sanitize dynamic comment data using htmlspecialchars to prevent XSS vulnerabilities

--- aksara/Modules/XHR/assets/js/comment.js
+++ aksara/Modules/XHR/assets/js/comment.js
@@ -121,17 +121,17 @@ $(document).ready(function () {
           const mentionHtml =
             typeof val.mention !== 'undefined' && val.mention
               ? `<div class="alert alert-warning callout p-2 mb-2">
-                ${phrase('Replying to')} <b>${val.mention.user || ''}</b>
+                ${phrase('Replying to')} <b>${htmlspecialchars(val.mention.user || '')}</b>
                 <br />
-                ${val.mention.comment || ''}
+                ${htmlspecialchars(val.mention.comment || '')}
               </div>`
               : '';
 
           const attachmentHtml =
             val.attachment && typeof val.attachment === 'object' && Object.keys(val.attachment).length
               ? `<div class="my-2">
-                <a href="${val.attachment.original || '#'}" class="d-block" target="_blank">
-                  <img src="${val.attachment.thumbnail || ''}" class="img-fluid rounded-4" style="max-width:320px" alt="${phrase('Attachment')}" loading="lazy" decoding="async" />
+                <a href="${htmlspecialchars(val.attachment.original || '#')}" class="d-block" target="_blank">
+                  <img src="${htmlspecialchars(val.attachment.thumbnail || '')}" class="img-fluid rounded-4" style="max-width:320px" alt="${phrase('Attachment')}" loading="lazy" decoding="async" />
                 </a>
               </div>`
               : '';
@@ -142,13 +142,13 @@ $(document).ready(function () {
 
           const menuItemsHtml = links.update_url
             ? `<li>
-                <a class="dropdown-item --modal" href="${links.update_url}">
+                <a class="dropdown-item --modal" href="${htmlspecialchars(links.update_url)}">
                   ${phrase('Update')}
                 </a>
               </li>`
             : links.report_url
               ? `<li>
-                <a class="dropdown-item --modal" href="${links.report_url}">
+                <a class="dropdown-item --modal" href="${htmlspecialchars(links.report_url)}">
                   ${phrase('Report')}
                 </a>
               </li>`
@@ -156,7 +156,7 @@ $(document).ready(function () {
 
           const hideItemHtml = links.hide_url
             ? `<li>
-                <a class="dropdown-item --modal" href="${links.hide_url}">
+                <a class="dropdown-item --modal" href="${htmlspecialchars(links.hide_url)}">
                   ${phrase('Visibility')}
                 </a>
               </li>`
@@ -167,7 +167,7 @@ $(document).ready(function () {
               ? `<div class="load-more-container row g-0">
                 <div class="col-12">
                   <div class="mb-3">
-                    <a href="${links.replies_url || window.location.href}" data-href="${links.replies_url || ''}" data-is-reply="1" class="load-more --fetch-comments text-body fw-bold">
+                    <a href="${htmlspecialchars(links.replies_url || window.location.href)}" data-href="${htmlspecialchars(links.replies_url || '')}" data-is-reply="1" class="load-more --fetch-comments text-body fw-bold">
                       <i class="mdi mdi-chevron-down"></i>
                       ${val.replies} ${val.replies > 1 ? phrase('Replies') : phrase('Reply')}
                     </a>
@@ -176,49 +176,53 @@ $(document).ready(function () {
               </div>`
               : '';
 
+          const fullName = `${val.first_name || ''} ${val.last_name || ''}`.trim();
+          const escapedFullName = htmlspecialchars(fullName);
+          const escapedCommentId = htmlspecialchars(val.comment_id || '');
+
           const itemHtml = `
             <div class="comment-item">
               <div class="d-flex">
                 <div class="flex-grow-0 pt-1">
-                  <a href="${links.profile_url || '#'}" class="--xhr">
-                    <img src="${val.photo || ''}" class="img-fluid rounded-circle" width="48" loading="lazy" decoding="async" />
+                  <a href="${htmlspecialchars(links.profile_url || '#')}" class="--xhr">
+                    <img src="${htmlspecialchars(val.photo || '')}" class="img-fluid rounded-circle" width="48" loading="lazy" decoding="async" />
                   </a>
                 </div>
                 <div class="flex-grow-1 ps-3">
                   <div class="d-flex align-items-center gap-1 comment-bubble">
                     <div class="bg-body-tertiary rounded-4 py-2 px-3 d-inline-block ${val.highlight ? 'border border-warning' : ''}">
                       <div class="comment-header">
-                        <a href="${links.profile_url || '#'}" class="text-body --xhr">
-                          <b id="comment-author-${val.comment_id}">
-                            ${val.first_name || ''} ${val.last_name || ''}
+                        <a href="${htmlspecialchars(links.profile_url || '#')}" class="text-body --xhr">
+                          <b id="comment-author-${escapedCommentId}">
+                            ${escapedFullName}
                           </b>
                         </a>
                         &middot;
                         <span class="text-muted">
-                          ${val.created_at || ''}
+                          ${htmlspecialchars(val.created_at || '')}
                         </span>
                       </div>
-                      <div id="comment-text-${val.comment_id}">
+                      <div id="comment-text-${escapedCommentId}">
                         ${mentionHtml}
                         ${commentContentHtml}
                       </div>
                     </div>
                     <div class="dropdown comment-dropdown flex-shrink-0">
-                      <button class="btn btn-link btn-sm text-body-secondary p-0" type="button" id="dropdownMenuButton${val.comment_id}" data-toggle="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                      <button class="btn btn-link btn-sm text-body-secondary p-0" type="button" id="dropdownMenuButton${escapedCommentId}" data-toggle="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="mdi mdi-dots-horizontal fs-5"></i>
                       </button>
-                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-right" aria-labelledby="dropdownMenuButton${val.comment_id}">
+                      <ul class="dropdown-menu dropdown-menu-end dropdown-menu-right" aria-labelledby="dropdownMenuButton${escapedCommentId}">
                         ${menuItemsHtml}
                         ${hideItemHtml}
                       </ul>
                     </div>
                   </div>
                   <div class="py-1 ps-3">
-                    <a href="${links.upvote_url || window.location.href}" data-href="${links.upvote_url || ''}" class="small text-body --upvote">
-                      <b><span id="comment-upvote-${val.comment_id}">${val.upvotes > 0 ? val.upvotes : ''}</span> ${phrase('Upvote')}</b>
+                    <a href="${htmlspecialchars(links.upvote_url || window.location.href)}" data-href="${htmlspecialchars(links.upvote_url || '')}" class="small text-body --upvote">
+                      <b><span id="comment-upvote-${escapedCommentId}">${val.upvotes > 0 ? val.upvotes : ''}</span> ${phrase('Upvote')}</b>
                     </a>
                     &middot;
-                    <a href="${links.reply_url || '#'}" class="small text-body --reply" data-profile-photo="${val.user_photo || ''}" data-mention="${val.first_name || ''} ${val.last_name || ''}">
+                    <a href="${htmlspecialchars(links.reply_url || '#')}" class="small text-body --reply" data-profile-photo="${htmlspecialchars(val.user_photo || '')}" data-mention="${escapedFullName}">
                       <b>${phrase('Reply')}</b>
                     </a>
                   </div>
@@ -244,7 +248,7 @@ $(document).ready(function () {
               <div class="col-12">
                 <div class="mb-3">
                   <p class="text-${is_reply ? 'start' : 'center'}">
-                    <a href="${response.next_page || window.location.href}" data-href="${response.next_page}" data-is-reply="${is_reply}" class="load-more --fetch-comments">
+                    <a href="${htmlspecialchars(response.next_page || window.location.href)}" data-href="${htmlspecialchars(response.next_page || '')}" data-is-reply="${is_reply}" class="load-more --fetch-comments">
                       <b>${is_reply ? phrase('Load more replies') : phrase('Load more comments')}</b>
                     </a>
                   </p>
